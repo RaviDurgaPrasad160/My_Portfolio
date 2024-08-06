925ca99e06dfd60103523fca23a81e5296e9d95f
Durga Prasad <ravidurgaprasad160@gmail.com>
2024-08-06 17:53:51 +0530

    comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $('.fa-bars').click(function(){
-        $(this).toggleClass("fa-bars fa-xmark")
-    })
-})
+const effs = document.querySelectorAll('.eff')
 
+window.addEventListener('scroll',checkEff) 
+
+function checkEff(){
+    let endLine = window.innerHeight * 0.67
+    effs.forEach((ef) => {
+        // console.log(ef.getBoundingClientRect().top) 
+        let effTop = ef.getBoundingClientRect().top
+        if(effTop < endLine){
+            ef.classList.add('show')
+        }else {
+            ef.classList.remove('show')
+        }
+    })   
+}
